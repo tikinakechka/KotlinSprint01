303299a125b654d6b4656c560de781d4c6c80048
tikinakechka <tikinakechka@gmail.com>
2025-01-28 18:54:30 +0300

    KS-1-3 Simplified the solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,14 @@
-data class Time(val hour: Int, val minute: Int)
-
-fun formatTime(hour: Int, minute: Int): String {
-    require(hour in 0..23) { "Hour must be between 0 and 23" }
-    require(minute in 0..59) { "Minute must be between 0 and 59" }
-    return String.format( "%02d:%02d", hour, minute)
-}
-
 fun main() {
-    val person = "Юрий Гагарин"
     val year = 1961
+    val launchHour = 9
+    val launchMinute = 7
 
-    val launchTime = Time(9, 7)
-    val landingTime = Time(10, 55)
+    var landingHour = 10
+    var landingMinute = 55
 
-    println("$person совершил полет в космос")
+    println("Юрий Гагарин совершил полет в космос")
     println("Год полета: $year")
-    println("Время взлета: ${formatTime(launchTime.hour, launchTime.minute)}")
-    println("Время посадки: ${formatTime(landingTime.hour, landingTime.minute)}")
+    println("Время взлета: $launchHour:${String.format("%02d", launchMinute)}")
+
+    println("Время посадки: $landingHour:${String.format("%02d", landingMinute)}")
 }
