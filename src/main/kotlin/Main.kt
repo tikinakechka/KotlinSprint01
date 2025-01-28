b8bf40237510eecb76f62293e38d249c5ac49976
tikinakechka <tikinakechka@gmail.com>
2025-01-28 20:41:43 +0300

    KS-2-2 The average salary is calculated

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
-package org.example
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val workers: Int = 50
+    val salary1: Int = 30000
+    val interns: Int = 30
+    val salary2: Int = 20000
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val expences1 = (workers * salary1)
+    val expences2 = (interns * salary2)
+
+    val totalExpences = (expences1 + expences2)
+    val totalWorkers = (interns + workers)
+
+    val average: Double = totalExpences.toDouble() / totalWorkers
+    val averageSalary = average.toInt()
+
+    println("Средняя зп работника после прихода стажеров: $averageSalary руб")
+    println("Расходы на постоянных сотрудников: $expences1 руб")
+    println("Общие расходы на зарплату с приходом стажеров: $totalExpences руб")
 }
