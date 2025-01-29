946f11b66d4596d820f820f6afee5aa5d22d3ead
tikinakechka <tikinakechka@gmail.com>
2025-01-29 16:41:32 +0300

    KS-1-2 logic was simplified

--- src/main/kotlin/task2.kt
+++ src/main/kotlin/task2.kt
@@ -1,18 +1,8 @@
 fun main() {
-    println("Введите количество заказов:")
-    val input = readLine()
-    val thx = "Спасибо за покупку!"
-    val totalOrdersCount: Int? = input?.toIntOrNull()
-    if (totalOrdersCount == null || totalOrdersCount <= 0) {
-        println("Некорректный ввод данных: количество заказов должно быть больше нуля.")
-        return
-    } else {
-        println("$totalOrdersCount")
-        println("$thx")
-    }
+    val orders: Int = 75
+    val thx: String = "Спасибо за покупку!"
     var totalEmployeesCount = 2000
     println("Количество работников: $totalEmployeesCount")
-
-    totalEmployeesCount -= 1
-    println("Количество работников после увольнения: $totalEmployeesCount")
+    var actualEmployeesCount = totalEmployeesCount - 1
+    println("Количество работников после увольнения: $actualEmployeesCount")
 }
